788c75b8d0b677ea286c85760458484e6d293e43
Pete Burns <peterburnshicks@gmail.com>
2021-07-09 01:43:31 -0400

    Updates

---
+++ precache-manifest.7ceea617069d54b76b5f7b3e3425be45.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5602c40d0186b24d0fbc6eb4568e1d41",
+    "url": "/keysim/index.html"
+  },
+  {
+    "revision": "6389bcec8e109e84fd71",
+    "url": "/keysim/static/css/main.2d7f26ea.chunk.css"
+  },
+  {
+    "revision": "ad764430ba32c38b2768",
+    "url": "/keysim/static/js/2.1f03a05c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/keysim/static/js/2.1f03a05c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6389bcec8e109e84fd71",
+    "url": "/keysim/static/js/main.2a7f0b70.chunk.js"
+  },
+  {
+    "revision": "f50eeb71513ab9ac5f47",
+    "url": "/keysim/static/js/runtime-main.a75ca373.js"
+  },
+  {
+    "revision": "3c4dafa94356c1bda9b876f25864758c",
+    "url": "/keysim/static/media/brushed-metal_albedo-512.3c4dafa9.png"
+  },
+  {
+    "revision": "42778e3977b6fa8d5b92554d160ce939",
+    "url": "/keysim/static/media/brushed-metal_roughness-512.42778e39.png"
+  },
+  {
+    "revision": "4cd74547fbe1cf4c3400067037c2c57f",
+    "url": "/keysim/static/media/icon_camera.4cd74547.svg"
+  },
+  {
+    "revision": "60a18049dd1dbb30bdf630e590a15f20",
+    "url": "/keysim/static/media/icon_check.60a18049.svg"
+  },
+  {
+    "revision": "04c6d9a23c71e6132a84b33388a7cb5d",
+    "url": "/keysim/static/media/icon_github.04c6d9a2.svg"
+  },
+  {
+    "revision": "10dca3f45aa8e99ec6c37efe64dc67c2",
+    "url": "/keysim/static/media/icon_link.10dca3f4.svg"
+  },
+  {
+    "revision": "554dc99ff8d220e376852801e69d6faa",
+    "url": "/keysim/static/media/icon_minus.554dc99f.svg"
+  },
+  {
+    "revision": "59d30865c3f7c7048962ca53a5e3dbb1",
+    "url": "/keysim/static/media/icon_plus.59d30865.svg"
+  },
+  {
+    "revision": "630d816d0c505fdec48b9cf728499a35",
+    "url": "/keysim/static/media/icon_search.630d816d.svg"
+  },
+  {
+    "revision": "1e7ea8c58747dcb82fdc0817a0cf6e7d",
+    "url": "/keysim/static/media/icon_select.1e7ea8c5.svg"
+  },
+  {
+    "revision": "df0585121c105835bcb73dbcc18be2b3",
+    "url": "/keysim/static/media/icon_x.df058512.svg"
+  },
+  {
+    "revision": "aa39265326d82a523bc8fb46238b613c",
+    "url": "/keysim/static/media/lightgold_albedo-512.aa392653.png"
+  },
+  {
+    "revision": "e3ed39a9379dbf6e4d02c921566ffc02",
+    "url": "/keysim/static/media/lightgold_roughness-512.e3ed39a9.png"
+  },
+  {
+    "revision": "a01c0bb198ac01fdf278970b5a36174a",
+    "url": "/keysim/static/media/logo.a01c0bb1.svg"
+  },
+  {
+    "revision": "7f4f5e6f3c6549596c7131b3eb5f8e90",
+    "url": "/keysim/static/media/logo_text.7f4f5e6f.svg"
+  },
+  {
+    "revision": "73cef08b5529faf07530d0c2a5449282",
+    "url": "/keysim/static/media/noise.73cef08b.png"
+  },
+  {
+    "revision": "0aab5a7c05e5505ef159a9777dc5a8fc",
+    "url": "/keysim/static/media/nx.0aab5a7c.jpg"
+  },
+  {
+    "revision": "bc1af0c163cac54c6e3f60c55d33a24d",
+    "url": "/keysim/static/media/ny.bc1af0c1.jpg"
+  },
+  {
+    "revision": "4f0800c2fcce326677e374372fb6cfd0",
+    "url": "/keysim/static/media/nz.4f0800c2.jpg"
+  },
+  {
+    "revision": "b64d2bde8f1f2a0b971814e403abcfa7",
+    "url": "/keysim/static/media/px.b64d2bde.jpg"
+  },
+  {
+    "revision": "7979b4b0f1b216e23d1cd807570b85fd",
+    "url": "/keysim/static/media/py.7979b4b0.jpg"
+  },
+  {
+    "revision": "2228829906c63f5b4f22e60c8ac9008e",
+    "url": "/keysim/static/media/pz.22288299.jpg"
+  }
+]);
